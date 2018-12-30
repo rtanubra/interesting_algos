25477b9b2ebee6f9da59e6d4cc3dbf270e097b52
rtanubra <rdtanubrata@gmail.com>
2018-12-30 10:50:41 -0500

    Added failing test cases for nCr

--- nCr Comp/test_nCr.py
+++ nCr Comp/test_nCr.py
@@ -4,8 +4,8 @@ import ncr
 class TestnCr(unittest.TestCase):
     
     def test_nCr_factorial(self):
-        inputs = [[20,12],[8,2],[15,5],[3,2],[4,2],[4,3]]
-        results = [125970,28,3003,3,6,4]
+        inputs = [[20,12],[8,2],[15,5],[3,2],[4,2],[4,3],[3,4],[-2,8]]
+        results = [125970,28,3003,3,6,4,"INVALID INPUTS PROVIDED","INVALID INPUTS PROVIDED"]
         for x, my_inp in enumerate(inputs):
             self.assertEqual(Factorial.recurrsive_factorial(inputs[x]),results[x])
     
