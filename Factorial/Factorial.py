1c320f22bda71b71e5c4eaf3c0ebc2ab29b91c47
Rey Tanubrata <rdtnaubrata@gmail.com>
2018-12-30 10:19:16 -0500

    Added iterative approach and further testing for factorials

--- Factorial/Factorial.py
+++ Factorial/Factorial.py
@@ -2,19 +2,32 @@ import time
 
 """
 This module is intended to explore the different methods to approach a factorial problem.
-Explores two recursive appraoches to compute factorials. 
 1. Primary recurrsive sequential approach
     -Slow approach
     -necessary aproach needs to be built to utilize for smaller numbers in fast doubling approach
+2. Primary iterative approach 
+    -Should have linear time complexity
 
 """
 
-def sequential_factorial(n):
+def recurrsive_factorial(n):
     #Traditional sequential approach
     if n == 1:
+        return 1
+    elif n==0:
         return 1
     elif n < 1:
         return "FAILED inappropriate input value"
     else:
-        return n * sequential_factorial(n-1)
+        return n * recurrsive_factorial(n-1)
         
+def iterative_factorial(n):
+    prod = 1
+    if n < 0: 
+        return "FAILED inappropriate input value"
+    elif n == 0 :
+        return 1
+    else:
+        for x in range(1,n+1):
+            prod *= x
+        return prod 
--- Factorial/test_Factorial.py
+++ Factorial/test_Factorial.py
@@ -3,10 +3,17 @@ import Factorial
 
 class TestFactorial(unittest.TestCase):
     
-    def test_sequential_factorial(self):
+    def test_recurrsive_factorial(self):
         #Looping through some relatively low numbers to test sequential factorial
-        inputs = [3,8,4,10,-1]
-        results = [6,40320,24,3628800,"FAILED inappropriate input value"]
+        inputs = [3,8,4,10,-1,1,0]
+        results = [6,40320,24,3628800,"FAILED inappropriate input value",1,1]
         for x, my_inp in enumerate(inputs):
-            self.assertEqual(Factorial.sequential_factorial(inputs[x]),results[x])
+            self.assertEqual(Factorial.recurrsive_factorial(inputs[x]),results[x])
+    
+    def test_iterative_factorial(self):
+        #Looping through some relatively low numbers to test sequential factorial
+        inputs = [3,8,4,10,-1,1,0]
+        results = [6,40320,24,3628800,"FAILED inappropriate input value",1,1]
+        for x, my_inp in enumerate(inputs):
+            self.assertEqual(Factorial.iterative_factorial(inputs[x]),results[x])
 
