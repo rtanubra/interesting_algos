aa045a13edee768f01618b50fc72208cd1bc2700
rtanubra <rdtanubrata@gmail.com>
2019-01-05 11:28:49 -0500

    Completed problem5 in recursion_madness/simple/problem5.py

--- Recursion_Madness/Simple/problem5.py
+++ Recursion_Madness/Simple/problem5.py
@@ -7,22 +7,54 @@ appearances of "pi" have been replaced by "3.14".
 """
 import unittest
 
+#Main function
 def change5_py(str):
-    pass
+    if len(str) <= 2:
+        if str == "Pi":
+            return "3.14"
+        else:
+            return str
+    else:
+        check_str = str[:1]
+        rest = str[1:]
+        if check_str == "P":
+            check_str = str[:2]
+            rest = str[2:]
+            if check_str == "Pi":
+                return "3.14"+change5_py(rest)
+            else:
+                return check_str+change5_py(rest)
+        else:
+            return check_str+change5_py(rest)
 
+#A method for simple testing through prints no unittest required   
+def mini_test():
+    inputs = [
+        "","PI","Pi",
+        "PIPijabPiberPi","PiabPiPibyPI",
+        "LPiorum",
+        "abby","Rey","Lorum"
+    ]
+    for x in inputs:
+        print(x)
+        print("obtained:",change5_py(x),"\n")
+
+#mini_test() #uncomment to view a mini test run.
+
+#Full test suite run through python3 -m unittest problem5.py
 class TestChange5_py(unittest.TestCase):
 
     def test_change5_multiplePi(self):
-        print("Testing strings that require a multiple change\n")
+        print("Testing strings that require multiple changes\n")
         inputs = ["PIPijabPiberPi","","PiabPiPibyPI","PiRePiyPi","LPiorumPi"]
-        ouputs = ["PI3.14jab3.14ber3.14","","3.14ab3.143.14byPI","3.14Re3.14y3.14","L3.14orum3.14"]
+        outputs = ["PI3.14jab3.14ber3.14","","3.14ab3.143.14byPI","3.14Re3.14y3.14","L3.14orum3.14"]
         for i,x in enumerate(inputs):
             self.assertEqual(change5_py(x),outputs[i])
 
     def test_change5_singlePi(self):
         print("Testing strings that require a single change\n")
         inputs = ["jabPiber","","Piabby","ReyPi","LPiorum"]
-        ouputs = ["jab3.14ber","","3.14abby","Rey3.14","L3.14orum"]
+        outputs = ["jab3.14ber","","3.14abby","Rey3.14","L3.14orum"]
         for i,x in enumerate(inputs):
             self.assertEqual(change5_py(x) , outputs[i])
 
@@ -32,3 +64,12 @@ class TestChange5_py(unittest.TestCase):
         inputs= ["jabber","","abby","Rey","Lorum"]
         for x in inputs:
             self.assertEqual( change5_py(x) , x)
+        
+    def test_change5_base_case(self):
+        #base case - len of string <=2
+        print("Testing base cases, strings of lenghts 2 or less")
+        inputs = ["","PI","Pi","Jo","J","R","P"]
+        outputs= ["","PI","3.14","Jo","J","R","P"]
+        for i,x in enumerate(inputs):
+            self.assertEqual(change5_py(x),outputs[i])
+
--- Recursion_Madness/Simple/readme.md
+++ Recursion_Madness/Simple/readme.md
@@ -21,7 +21,7 @@ This directory wills store several recursion problems and their tests. Recursion
 
     Given a non-negative int n, return the count of the occurrences of 7 as a digit, so for example 717 yields 2. (no loops). Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
 
-5. ChangePi
+5. ChangePi - completed 
 
     Given a string, compute recursively (no loops) a new string where all appearances of "pi" have been replaced by "3.14".
 
